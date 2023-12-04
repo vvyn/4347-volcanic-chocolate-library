0352254717976ef40ee78cd0fa42b2146b554f6a
vvyn <vicavyn3@gmail.com>
2023-12-03 21:55:39 -0600

    change file organization for submission

---
+++ project/create_librarian.php
@@ -0,0 +1,42 @@
+<?php
+$sqlQuery = "";
+$message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $con = mysqli_connect('localhost', 'root', '', 'db_library');
+
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    // fname, lname, employeeSsn, birthday, address, salary, 
+
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $employeeSsn = $_POST['employeeSsn'];
+    $birthday = $_POST['birthday'];
+    $address = $_POST['address'];
+    $salary = $_POST['salary'];
+
+    $sqlQuery = "INSERT INTO `librarian` (`Fname`, `Lname`, `EmployeeSSN`, `Bdate`, `Address`, `Salary`) 
+    VALUES ('$fname', '$lname', '$employeeSsn', '$birthday', '$address', '$salary')";
+
+    $result = mysqli_query($con, $sqlQuery);
+
+     if ($result) {
+        $message = "Librarian has been created successfully.";
+    } else {
+        $message = "Error creating book: " . mysqli_error($con);
+    }
+    
+
+    mysqli_close($con);
+}
+
+if (!empty($sqlQuery)) {
+    echo "<p><strong>SQL Query:</strong> " . htmlspecialchars($sqlQuery) . "</p>";
+}
+
+if (!empty($message)) {
+    echo "<p><strong>Results:</strong><br> " . $message . "</p>";
+}
+?>
